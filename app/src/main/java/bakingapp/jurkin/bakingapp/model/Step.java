f934ecba6bbf040ce0bb36846c79c42f3baa0a64
Andrej Jurkin <andrej.jurkin@gmail.com>
2017-07-09 10:32:49 +0200

    Add model classes

---
+++ app/src/main/java/bakingapp/jurkin/bakingapp/model/Step.java
@@ -0,0 +1,78 @@
+/*
+ *
+ *  * Copyright 2017 Andrej Jurkin
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package bakingapp.jurkin.bakingapp.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Andrej Jurkin on 7/9/17.
+ */
+
+public class Step {
+
+    private int id;
+    private String shortDescription;
+    private String description;
+
+    @SerializedName("videoURL")
+    private String videoUrl;
+
+    @SerializedName("thumbnailURL")
+    private String thumbnailUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+}
